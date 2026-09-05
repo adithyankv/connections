0262660082abb80796270039b25e0b44d1fb1b4a
Adithyan K V <29067795+adithyankv@users.noreply.github.com>
2026-09-05 22:40:06 +0530

    Add hover and click mechanics
    - Add hover and click mechanics to tiles

--- main.py
+++ main.py
@@ -17,10 +17,24 @@ def main():
         "Create four groups of four", True, "#999999"
     )
     grid = Grid()
+    selected_count = 0
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                for tile in grid.tiles:
+                    if tile.rect.collidepoint(pygame.mouse.get_pos()):
+                        if not tile.selected and selected_count < 4:
+                            tile.selected = True
+                            selected_count += 1
+                        elif tile.selected:
+                            tile.selected = False
+                            selected_count -= 1
+                        print(selected_count)
+
+        for tile in grid.tiles:
+            tile.hovered = tile.rect.collidepoint(pygame.mouse.get_pos())
 
         screen.fill("#FFFFFF")
         screen.blit(connections_text, connections_text.get_rect(topleft=(32, 32)))
@@ -31,6 +45,7 @@ def main():
             screen_mid_x - (grid.size[0] // 2),
             screen_mid_y - (grid.size[1] // 2),
         )
+
         pygame.display.flip()
 
     pygame.quit()
@@ -41,30 +56,45 @@ class Tile:
         self.box_size = 96
         self.text = "TEXT"
         # color picked to match NYT connections
-        self.tile_color = "#EEEEE6"
+        self.hovered_color = "#DDDDD6"
+        self.base_color = "#EEEEE6"
+        self.selected_color = "#5A584E"
+
+        self.color = self.base_color
+        self.rect = pygame.Rect()
+        self.selected = False
+        self.hovered = False
 
     def draw(self, surface, x: int, y: int) -> None:
+        self.rect = pygame.Rect(x, y, self.box_size, self.box_size)
+        if self.hovered and not self.selected:
+            self.color = self.hovered_color
+        elif self.selected:
+            self.color = self.selected_color
+        else:
+            self.color = self.base_color
+
         pygame.draw.rect(
             surface,
-            self.tile_color,
-            pygame.Rect(x, y, self.box_size, self.box_size),
+            self.color,
+            self.rect,
             border_radius=6,
         )
 
 
 class Grid:
     def __init__(self) -> None:
-        self.tiles = [[Tile() for i in range(4)] for _ in range(4)]
+        self.tiles = [Tile() for _ in range(16)]
         self.grid_spacing = 10
         self.size = (
-            4 * (self.tiles[0][0].box_size + self.grid_spacing),
-            4 * (self.tiles[0][0].box_size + self.grid_spacing),
+            4 * (self.tiles[0].box_size + self.grid_spacing),
+            4 * (self.tiles[0].box_size + self.grid_spacing),
         )
 
     def draw(self, surface, x, y):
         for row in range(0, 4):
             for column in range(0, 4):
-                tile = self.tiles[row][column]
+                tile = self.tiles[row * 4 + column]
                 tile.draw(
                     surface,
                     x + row * (tile.box_size + self.grid_spacing),
